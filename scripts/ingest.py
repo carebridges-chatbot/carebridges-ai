3d9e4545418297dfdb58c0252a76ec0d09b29a0f
SeoyoungKim <sy1214ei@gmail.com>
2025-08-07 20:04:29 +0900

    feat: ingest.py에 FAQ pdf 파일 한번에 갖고오는 반복문 구현

--- scripts/ingest.py
+++ scripts/ingest.py
@@ -1,29 +1,38 @@
 import os
 from dotenv import load_dotenv
-load_dotenv() # .env 파일 로드됨
-
+from pathlib import Path
 from split_documents import split_pdf
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 
+load_dotenv()
+
 # 경로 설정
-PDF_PATH = "data/제1장_장기요양인정_신청.pdf (248037 Bytes)"
+PDF_DIR = Path("data/FAQ")
 FAISS_SAVE_PATH = "db/faiss_index"
 
 def main():
+    all_chunks = []
+
     print("PDF에서 텍스트 분할 중...")
-    chunks = split_pdf(PDF_PATH)
-    print(f"총 {len(chunks)}개의 청크 생성 완료")
+    for pdf_file in PDF_DIR.glob("*.pdf"):
+        print(f"-> cjflwnd: {pdf_file.name}")
+        chunks = split_pdf(str(pdf_file))
+        print(f"    -{len(chunks)}개 청크 생성 완료")
+        all_chunks.extend(chunks)
+        
+    
+    print(f"\n총 {len(all_chunks)}개의 청크 생성 완료")
 
     print("OpenAI 임베딩 모델 로드 중...")
     embeddings = OpenAIEmbeddings()
 
     print("FAISS 벡터스토어 생성 중...")
-    vectorstore = FAISS.from_documents(chunks, embedding=embeddings)
+    vectorstore = FAISS.from_documents(all_chunks, embedding=embeddings)
 
     print(f"FAISS 벡터스토어를 '{FAISS_SAVE_PATH}'에 저장 중...")
     vectorstore.save_local(FAISS_SAVE_PATH)
-    print("벡터 DB 생성 완료!")
+    print("전체 PDF 임베딩 및 저장 완료!")
 
 if __name__ == "__main__":
     main()
